81692fb53a1cca971f6611094d3d9bc4de68b585
Lahiru Bandara <73198791+Lahiru-Chathumina@users.noreply.github.com>
2024-11-23 22:47:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const text = "Hi 👋, I'm Lahiru Bandara"; 
+    let index = 0; 
+    const typingSpeed = 150; 
+    const deletingSpeed = 50; // Speed at which the text will be deleted
+    const targetElement = document.getElementById('autoTypingText');
+    let isDeleting = false;
+
+    function typeText() {
+        if (!isDeleting) {
+            if (index < text.length) {
+                targetElement.innerHTML += text.charAt(index); 
+                index++;
+                setTimeout(typeText, typingSpeed); 
+            } else {
+                isDeleting = true;
+                setTimeout(typeText, 1000); // Wait a second before starting to delete
+            }
+        } else {
+            if (index > 0) {
+                targetElement.innerHTML = text.substring(0, index - 1); 
+                index--;
+                setTimeout(typeText, deletingSpeed); 
+            } else {
+                isDeleting = false;
+                setTimeout(typeText, 1000); // Wait a second before typing again
+            }
+        }
+    }
+
+    typeText(); // Start the typing effect
+});
